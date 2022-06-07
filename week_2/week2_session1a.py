e9dbb4d45e6dcbc35d22f1cccfe432192609d41e
Kevin <kev.chu.pro@gmail.com>
2022-06-07 09:25:16 -0700

    feat: week 2 problems

---
+++ week_2/week2_session1a.py
@@ -0,0 +1,22 @@
+"""
+Problem 1 - Element with a frequency of K
+Find the element that appears k number of times in an array.
+
+Examples:
+
+Input: [8, 7, 9, 6, 7, 5, 1], k = 2
+Output: 7
+"""
+
+"""
+UMPIRE  
+Understand what the interviewer is asking for by using test cases and questions about the problem.  
+Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.  
+Plan the solution with appropriate visualizations and pseudocode.  
+Implement the code to solve the algorithm.  
+Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.  
+Evaluate the performance of your algorithm and state any strong/weak or future potential work.  
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ week_2/week2_session2a.py
@@ -0,0 +1,22 @@
+"""
+Problem 1: Find a pair with given sum
+Given an array of size n and a number x, determine the first two elements in the array, if any, whose sum is exactly x.
+
+Examples:
+
+Input: [3, 2, 6, 9, 5], 9
+Output: (3, 6)
+"""
+
+"""
+UMPIRE  
+Understand what the interviewer is asking for by using test cases and questions about the problem.  
+Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.  
+Plan the solution with appropriate visualizations and pseudocode.  
+Implement the code to solve the algorithm.  
+Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.  
+Evaluate the performance of your algorithm and state any strong/weak or future potential work.  
+"""
+
+if __name__ == "__main__":
+    pass
