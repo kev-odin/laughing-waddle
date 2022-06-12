ed20235270f2c1ae6c1ef69f95addbcb7885b51d
baottn <nttb04121999@gmail.com>
2022-06-11 16:43:20 -1000

    add UMPIRE to week2_session2b

--- week_2/week2_session2b.py
+++ week_2/week2_session2b.py
@@ -42,5 +42,43 @@ Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
 
+'''
+Understanding:
+_ Convert Roman numerals to Arabic numerals
+_ Some numbers such as 4, 9 are IV and IX not IIII and VIIII
+_ What if the input is IIII? Should we return 4 or 2 & 3 or 3 & 2 -> assume that all the inputs are valid
+_ What if the input is an empty string? Assume the string is not empty
+_ What is the space constraint?
+_ WHat is the time constraint?
+
+Match:
+_ Dictionary, HashMap, array
+
+Plan:
+_ Initilize a dictionary to store all the Roman numbers as keys and Arabic numerals as values.
+_ Make a sum variable to store the sum
+_ Loop from the end of the string input and check the next character. If the next character is less than 
+the current chacacter, subtract it from the current character and add to the sum, otherwise, just convert
+and add every character to the sum
+
+Implement: see below
+
+Review:
+
+Evaluate:
+*Time complexity : O(n)
+*Space complexity : O(n)
+'''
+
+def convertNumbers(s):
+    numSum = 0
+    numDict = {'I' : 1, 'V' : 5, 'X' : 10 , 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    for i in range(len(s) - 1, 0, -1):
+        if numDict(s[i]) <= numDict(s[i - 1]):
+            numSum += numDict(s[i])
+        else:
+            numSum += numDict(s[i]) - numDict(s[i - 1])
+    return numSum
+
 if __name__ == "__main__":
     pass
