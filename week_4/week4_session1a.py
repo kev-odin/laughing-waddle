ca700152f29be90c1ef5a566e9b088ef0302a15d
Kevin <kev.chu.pro@gmail.com>
2022-06-30 12:47:52 -0700

    learn: week 4 umpire

--- week_4/week4_session1a.py
+++ week_4/week4_session1a.py
@@ -1,14 +1,10 @@
 """
 Problem 1: Remove Nth Node from End of List
 Given a linked list, remove the n-th node from the end of list and return its head.
-
 Example:
-
 Input: 1->2->3->4->5, n = 2
 Output: 1->2->3->5
-
 Explanation: We remove the second node from the end, the node with value 4
-
 UMPIRE  
 Understand what the interviewer is asking for by using test cases and questions about the problem.  
 Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.  
@@ -16,17 +12,46 @@ Plan the solution with appropriate visualizations and pseudocode.
 Implement the code to solve the algorithm.  
 Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.  
 Evaluate the performance of your algorithm and state any strong/weak or future potential work.
-
 U: 
-    bao:
-    far:
+    bao: is there a cycle?
+        - are there constraints to n's value? negative values?
+    far: is the list empty?
     jes:
-    kev:
-    tin:
-    xin:
+    kev: what are we expected to return? - return the head :)
+    tin: what if there are two same values? - remove both or just one?
+        - position of the node from the last node
+    xin: when n is greater than the length of the linked list, will it go backwards?
+    
+testcase:
+- single node; n = 1, 0? bad inputs
+- cycle one: 1 -> 2 -> 3 -> 4 -> 1 - no way to find the end 
+-  1->2->3->4->5, n = 2, 5
 M:
+    assume: n will not be larger than the length of the list 
+    two pointers -> fast and slow pointers, keep the distance between the fast and slow pointer to be n
+    - moving in the same speed? - moving the same speed, fast: will have distance, distance will be n
+    - two passes: getting the length of the list (length - n +/- 1), finding the need to delete value: two pointers
+    - dummy node?
+    - 
 P: 
+make two pointers:
+make dummy node pointing to head?
+for loop in range of n:
+    update fastpointer 
+    if fastpointer == null:
+       return dummyheadnode.next
+       
+while loop to move fastpointer.next is not null:
+    update slowpointer, fastpointer
+make slowpointer.next equal the next next value
+
+return the dummyheadnode :)
+
+
+
 I:
 R:
 E: 
+time complex: O(n)
+space complex: O(1)?
 """
--- week_4/week4_session1c.py
+++ week_4/week4_session1c.py
@@ -1,16 +1,11 @@
 """
 Problem #3: Add Two Numbers
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
 Example:
-
 Input: 2 -> 4 -> 3,  5 -> 6 -> 4
 Output: 7 -> 0 -> 8
-
 Explanation: 342 + 465 = 807
-
 UMPIRE  
 Understand what the interviewer is asking for by using test cases and questions about the problem.  
 Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.  
@@ -18,17 +13,36 @@ Plan the solution with appropriate visualizations and pseudocode.
 Implement the code to solve the algorithm.  
 Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.  
 Evaluate the performance of your algorithm and state any strong/weak or future potential work.
-
 U: 
-    bao:
+    bao:Can the output list be longer the input list? Should we make a new node for the last carry over or should we 
+    trash it?
     far:
-    jes:
-    kev:
+    jes:Is there a possibility of an empty list?
+    -What will happen if there is a carry over for every digit?
+    kev:What if the length of the lists are not the same? 
     tin:
     xin:
 M:
+-Linked list!!
+-two pointers
+-dummy nodes to store the sum of the two lists
+-multiple passes to get the lengths? maybe
+
 P: 
+Create a dummy node to point to the head
+initiate the two pointers to point to the head of the lists
+create a current node 
+initiate a variable to store the carry over value
+Traverse the two lists while the pointers are not null
+  -get the sum of each two pointer values
+  -get the carry over values
+  -if there is a carry over connect it to the end of the list
+ return dummy.next
+
+
 I:
 R:
 E: 
+time complexity : O(M + N)
+space complexity: O(N + 1) = O(N)  - N being longest list length 
 """
