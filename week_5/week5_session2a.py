bbf1901101cff0754337fdab927469b2368d94ad
Kevin <kev.chu.pro@gmail.com>
2022-06-30 12:44:05 -0700

    learn: week 5!

---
+++ week_5/week5_session2a.py
@@ -0,0 +1,44 @@
+"""
+Problem #1: Evaluate Postfix expression
+Write a function to evaluate the value of an expression in postfix notation represented by a string with numbers between 0 and 9 and operators separated by whitespace. The expression supports 4 binary operators '+', '*', '-' and '/'.
+
+Note: Arithmetic Expressions can be written in Infix, Prefix or Postfix notations.
+
+Infix notations are human readable form of expression. For example: “3 + 4”.
+Prefix notations has operators before the operands like “+ 3 4” for the previous infix example.
+Postfix notations keep operators after operands. A postfix expression for the examples above would be “3 4 +”.
+Play with this infix, prefix and postfix notations on this infix and postfix converter and build an intuition.
+
+Example:
+
+Input: "5 1 2 + 4 * + 3 -"
+Output: 14
+
+The expression is evaluated as follows:
+5 3 4 * + 3 -
+5 12 + 3 -
+17 3 -
+14
+
+UMPIRE  
+Understand what the interviewer is asking for by using test cases and questions about the problem.  
+Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.  
+Plan the solution with appropriate visualizations and pseudocode.  
+Implement the code to solve the algorithm.  
+Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.  
+Evaluate the performance of your algorithm and state any strong/weak or future potential work.
+
+U: 
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P: 
+I:
+R:
+E: 
+
+"""
---
+++ week_5/week5_session2b.py
@@ -0,0 +1,36 @@
+"""
+Problem #2: Valid Parentheses
+Given a string s containing characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Input: s = "()"
+Output: true
+
+Input: s = "(]"
+Output: false
+
+UMPIRE  
+Understand what the interviewer is asking for by using test cases and questions about the problem.  
+Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.  
+Plan the solution with appropriate visualizations and pseudocode.  
+Implement the code to solve the algorithm.  
+Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.  
+Evaluate the performance of your algorithm and state any strong/weak or future potential work.
+
+U: 
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P: 
+I:
+R:
+E: 
+
+"""
