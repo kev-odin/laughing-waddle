40cdacceca3688539e34ba64058894bdaff60470
Kevin <kev.chu.pro@gmail.com>
2022-07-07 13:24:03 -0700

    learn: added quesions

---
+++ week_6/week6_session2a.py
@@ -0,0 +1,49 @@
+"""
+Problem 1 - Moving Average from Data Stream
+
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Implement the MovingAverage class:
+
+    MovingAverage(int size): Initializes the object with the size of the window size.
+    double next(int val): Returns the moving average of the last size values of the stream.
+
+Examples
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+
+UMPIRE
+Understand what the interviewer is asking for by using test cases and questions about the problem.
+Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.
+Plan the solution with appropriate visualizations and pseudocode.
+Implement the code to solve the algorithm.
+Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.
+Evaluate the performance of your algorithm and state any strong/weak or future potential work.
+
+U:
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P:
+I:
+R:
+E:
+"""
+
+if __name__ == "__main__":
+    pass
---
+++ week_6/week6_session2b.py
@@ -0,0 +1,52 @@
+"""
+Problem 2 - Queue using Stacks
+
+We are given a stack data structure that supports standard operations like (push, pop, peek, and empty), implement a queue using instances of stack data structure and operations on them.
+
+Implement a Queue class with the following methods:
+
+    void push(int x): Pushes element x to the back of the queue.
+    int pop(): Removes the element from the front of the queue and returns it.
+    int peek(): Returns the element at the front of the queue.
+    boolean empty(): Returns true if the queue is empty, false otherwise.
+
+Example:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+
+UMPIRE
+Understand what the interviewer is asking for by using test cases and questions about the problem.
+Match what this problem looks like to known categories of problems, e.g. Linked List or Dynamic Programming and strategies or patterns in those categories.
+Plan the solution with appropriate visualizations and pseudocode.
+Implement the code to solve the algorithm.
+Review the code by running specific example(s) and recording values (watchlist) of your code's variables along the way.
+Evaluate the performance of your algorithm and state any strong/weak or future potential work.
+
+U:
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P:
+I:
+R:
+E:
+"""
+
+if __name__ == "__main__":
+    pass
