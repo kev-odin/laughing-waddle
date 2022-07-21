532cad8e68a294e28e1db877e33da386d741cba3
Kevin <kev.chu.pro@gmail.com>
2022-07-21 13:05:50 -0700

    learn: added problems for session 2 week 8

---
+++ week_8/week8_session2a.py
@@ -0,0 +1,28 @@
+"""
+Problem #1: Kth Smallest Element
+
+Given an array and a number k where k is less than size of array, we need to find the 
+k th smallest element in the given array. It is given that all array elements are distinct.
+
+Examples:
+
+Input: [7, 10, 4, 3, 20, 15] k = 3
+Output: 7
+
+Input: [7, 10, 4, 3, 20, 15] k = 4
+Output: 10
+
+U:
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P:
+I:
+R:
+E:
+
+"""
