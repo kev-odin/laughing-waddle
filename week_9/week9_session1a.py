ac8aef54310ffdd78dd52291fbe7b432015baf26
baottn <nttb04121999@gmail.com>
2022-07-26 15:16:48 -0700

    Upload week9_session1a solution

--- week_9/week9_session1a.py
+++ week_9/week9_session1a.py
@@ -1,7 +1,8 @@
 """
 Problem 1: Stack using Queues
 
-Given a Queue data structure that supports standard operations like push, top, pop, and empty. Implement a Stack data structure using only instances of Queue and queue operations allowed on the instances.
+Given a Queue data structure that supports standard operations like push, top, pop, and empty. 
+Implement a Stack data structure using only instances of Queue and queue operations allowed on the instances.
 
 Implement a Stack class:
 
@@ -11,7 +12,6 @@ Implement a Stack class:
     boolean empty(): Returns true if the stack is empty, false otherwise.
 
 Example:
-
 Input
 ["MyStack", "push", "push", "top", "pop", "empty"]
 [[], [1], [2], [], [], []]
@@ -26,17 +26,69 @@ myStack.top(); // return 2
 myStack.pop(); // return 2
 myStack.empty(); // return False
 
-U:
-    bao:
-    far:
-    jes:
-    kev:
-    tin:
-    xin:
-M:
-P:
-I:
-R:
-E:
+2:46PM - 3:01PM
+
+Understanding:
+Implement a Stack based on Queue 
+Only return when top or pop
+return T or F when empty is called
+not returning anything when push
+1, 2
+
+Match:
+deque -> double ended queue
+
+Plan:
+Initialize a Stack class
+Intilize a queue as deque inside of Stack
+Implement the push function by using appending the value to the queue
+Implement the empty function by checking if the queue is empty or not
+Implement the pop function by dequeuing all the elements in the initial queue to the auxillary queue except
+of the last element. Then deque and return the last element.
+Implement the top function by dequeing all the elements in the initial queue to the auxillary queue except
+of the last element. Then store the last element and then dequue to the aux queue and return the stored element afterward
+I would make the current queue to be the aux_queue then make new aux_queue
+
+queue =  
+aux_queue = 1, 2
+queue = aux_queue
+aux_queue = []
+
+Implement:
+Review:
+
 
 """
+from collections import deque
+class Stack()
+    def __init__(self):
+        self.queue = deque()
+        self.aux_queue = deque()
+        
+    def push(self, val):
+        self.queue.append(val)
+        
+    def empty(self):
+        if self.queue:
+            return True
+        else:
+            return False
+            
+    def pop(self, val):
+        while len(self.queue) >= 1:
+            self.aux_queue.append(self.queue.popleft())
+        topStack = self.queue.popleft()
+        queue = aux_queue
+        aux_queue = deque()
+        return topStack
+
+    def top(self, val):
+        while len(self.queue) >= 1:
+            self.aux_queue.append(self.queue.popleft())
+        topStack = self.queue.popleft()
+        self.aux_queue.append(topStack)
+        queue = aux_queue
+        aux_queue = deque()
+        return topStack
+    
+            
