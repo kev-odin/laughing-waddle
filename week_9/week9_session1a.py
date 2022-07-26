a515d407951571a2432b8a7bb7b406965fcc912f
Kevin <kev.chu.pro@gmail.com>
2022-07-26 14:11:33 -0700

    learn: adding interview questions

---
+++ week_9/week9_session1a.py
@@ -0,0 +1,42 @@
+"""
+Problem 1: Stack using Queues
+
+Given a Queue data structure that supports standard operations like push, top, pop, and empty. Implement a Stack data structure using only instances of Queue and queue operations allowed on the instances.
+
+Implement a Stack class:
+
+    void push(int x): Pushes element x to the top of the stack.
+    int pop(): Removes the element on the top of the stack and returns it.
+    int top(): Returns the element on the top of the stack.
+    boolean empty(): Returns true if the stack is empty, false otherwise.
+
+Example:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+
+U:
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P:
+I:
+R:
+E:
+
+"""
---
+++ week_9/week9_session1b.py
@@ -0,0 +1,29 @@
+"""
+Problem 2: Reverse Words in a String II
+
+Write a method reverseWords() that takes a message as an array of characters and reverses the order of the words in place.
+
+Input:
+  char[] message = ['c', 'a', 'k', 'e', ' ',
+                   'p', 'o', 'u', 'n', 'd', ' ',
+                   's', 't', 'e', 'a', 'l' ];
+                   
+Output: [ 's', 't', 'e', 'a', 'l', ' ',
+          'p', 'o', 'u', 'n', 'd', ' ',
+          'c', 'a', 'k', 'e'];
+
+
+U:
+    bao:
+    far:
+    jes:
+    kev:
+    tin:
+    xin:
+M:
+P:
+I:
+R:
+E:
+
+"""
