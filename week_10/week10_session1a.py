1eaeeb79ac4a86a787e8b99991d4f7193f513634
wongj7321 <70126037+wongj7321@users.noreply.github.com>
2022-08-02 18:35:07 -0400

    session1a

--- week_10/week10_session1a.py
+++ week_10/week10_session1a.py
@@ -20,20 +20,39 @@ Input: p = [1,2,1], q = [1,1,2]
 Output: false
 
 U:
-    bao:
-    far:
-    jes:
-    kev:
-    tin:
-    xin:
+    bao:What if both trees are null? would it be true or false?
+    far:Do we know if it is BST or not? No we don't
+    jes:What is the maximum depth of the tree? (0-100)
+    kev:Do we have any memory restriction with the solution?recursive? iteretive?
+    Can we assume the tree is complete?
+    tin:how height of trees can we expect? one node at least(height is at least one)
+    xin:Agree 
 M:
+Preorder traversal
+Recurssion
+
 P:
+-base case: check if the nodes are the same 
+-check if leaf nodes of the trees are the same
+  if not return false
+  else return true
+  
+
 I:
 R:
 E:
-
 """
-
-
+def sameTree(node1, node2):
+    if (node1 and node2) is None:
+        return True
+        
+    if not node1 or not node2:
+        return False
+        
+    if node1.val != node2.val:
+        return False
+        
+    return sameTree(node1.right, node2.right) and sameTree(node1.left, node2.left)
+    
 if __name__ == "__main__":
     pass
