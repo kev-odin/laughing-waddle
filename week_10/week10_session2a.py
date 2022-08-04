33d6a5c5c25153e7d5a2a37d47d502ee09c67ea9
Kevin <kev.chu.pro@gmail.com>
2022-08-04 13:42:05 -0700

    learn: added basic trees

--- week_10/week10_session2a.py
+++ week_10/week10_session2a.py
@@ -24,5 +24,48 @@ E:
 """
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"{self.val}"
+
+
 if __name__ == "__main__":
-    pass
+    """
+    Basic Tree:
+                1
+               / \
+              2   3
+             /     \
+            4       5
+    Another Tree:
+                1
+               / \
+              2   3
+             / \
+            4   5
+    Best Tree:
+                1
+               / \
+              2   3
+             / \   \
+            4   5   6
+
+    """
+    basic = TreeNode(
+        1, left=TreeNode(2, left=TreeNode(4)), right=TreeNode(3, right=TreeNode(5))
+    )
+
+    another = TreeNode(
+        1, left=TreeNode(2, left=TreeNode(4), right=TreeNode(5)), right=TreeNode(3)
+    )
+
+    best = TreeNode(
+        1,
+        left=TreeNode(2, left=TreeNode(4), right=TreeNode(5)),
+        right=TreeNode(3, right=TreeNode(6)),
+    )
