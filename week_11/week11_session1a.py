146d2d58cc32dd63e87d9e9ab14b61b2cd3c9ffd
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2022-08-09 15:49:46 -0700

    Update week11_session1a.py

--- week_11/week11_session1a.py
+++ week_11/week11_session1a.py
@@ -30,20 +30,38 @@ Another accepted tree is: [5, 2, 7, 1, 3, null, null, null, 4]
              4  
 
 U:
-    bao:
-    far:
-    jes:
-    kev:
-    tin:
-    xin:
+    Bao: Is the tree balanced?
+    Kevin: Are values in the tree unique?
+    Jessica: Is the tree completed?
+    Farnoosh: Please feel free to add more questions
+    Xingguo: Please feel free to add more questions
+    Tin: What if the root is empty? Then, ignore null. Create a new node and return the value.
 M:
+- Recursion
+- DFS (Bao: PostOrder) (Kevin: PreOrder)
 P:
-I:
+    Check the root is None or not:
+        - If root is None:
+            return new node(value)
+    Check value if it is less or greater than root node:
+        - If the value greater than root:
+            - Go right and insert the value
+        - Else:
+            - Go left and insert the value
+    return root
+I: Shown below
 R:
-E:
-
+E: 
+    Time: O(log N) if balanced, unbalanced O(N)
+    Space: O(log N) if balancedl, unbalanced O(N)
+             
 """
 
-
-if __name__ == "__main__":
-    pass
+def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    if root is None:
+        return TreeNode(val)
+    if val > root.val:
+        root.right = self.insertIntoBST(root.right, val)
+    else:
+        root.left = self.insertIntoBST(root.left, val)
+    return root
